8a0a464a4c2985f86aaa8e181631d40dc9330424
Dak <zawulf18@gmail.com>
2020-08-31 09:30:15 -0600

    Refactoring to Arrow Functions

--- nodejs-study-app/app.js
+++ nodejs-study-app/app.js
@@ -23,7 +23,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -44,7 +44,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) { 
+    handler(argv) { 
         notes.removeNote(argv.title, argv.body)
     }
 })
@@ -53,7 +53,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
+    handler() {
         console.log('Reading the note!')
     }
 })
--- nodejs-study-app/notes.js
+++ nodejs-study-app/notes.js
@@ -3,16 +3,17 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function () {
+const getNotes = () => {
     return 'Your notes...'
 }
 
 // This will check to see if the data inputed will be a duplicate or not.
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    // const duplicateNotes = notes.filter(function (note) {
+    //     return note.title === title
+    // })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -26,11 +27,12 @@ const addNote = function (title, body) {
     }
 }
 
-const removeNote = function (title, body) {
+const removeNote = (title, body) => {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title
-    })
+    // const notesToKeep = notes.filter(function (note) {
+    //     return note.title !== title
+    // })
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length > notesToKeep.length) {
         console.log(chalk.green.inverse('Note removed!'))
@@ -40,13 +42,13 @@ const removeNote = function (title, body) {
     }
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
 // This will fail without the file not being added previously!
-const loadNotes = function () {
+const loadNotes = () => {
     try {   
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
