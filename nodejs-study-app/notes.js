3b5a11cd5e5188f38083764d79de9e5ff537df0b
Dak <zawulf18@gmail.com>
2020-08-31 07:28:25 -0600

    Adding a Note 1

--- nodejs-study-app/notes.js
+++ nodejs-study-app/notes.js
@@ -1,9 +1,40 @@
-// I did it wrong...
-// const getNotes = 'Your Notes'
+// This is a barebone file that allows us to automatically create a notes.json file and upload data to it via command line.
+
+const fs = require('fs')
 
 const getNotes = function () {
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+    const notes = loadNotes()
+
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    saveNotes(notes)
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+// This will fail without the file not being added previously!
+const loadNotes = function () {
+    try {   
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
 
