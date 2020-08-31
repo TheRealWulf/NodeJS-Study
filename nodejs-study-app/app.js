ea2579fc2e226d79703a3dc76eea9400f8562b39
Dak <zawulf18@gmail.com>
2020-08-31 08:36:38 -0600

    Removing a Note

--- nodejs-study-app/app.js
+++ nodejs-study-app/app.js
@@ -32,8 +32,20 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing the note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) { 
+        notes.removeNote(argv.title, argv.body)
     }
 })
 
