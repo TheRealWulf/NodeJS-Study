888b92dc809636b9767f405c73fa6ac8aa479ec0
Dak <zawulf18@gmail.com>
2020-08-31 07:08:59 -0600

    Storing Data with JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -12,3 +12,13 @@ const fs = require('fs')
 // const data = JSON.parse(dataJSON)
 // console.log(data.title)
 
+// Challenge: Work with JSON and the file system.
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = 'Rick'
+user.age = 56
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
