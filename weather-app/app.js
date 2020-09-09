7f6e594409d4feeabd873fa63fc208782449592e
Dak <zawulf18@gmail.com>
2020-09-09 10:44:17 -0600

    Weather Description/Temperature

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,9 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=e9d0ddc1da3a4f450a3e7e4e0c247501&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=e9d0ddc1da3a4f450a3e7e4e0c247501&query=37.8267,-122.4233&units=f';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current);
+    console.log("The weather is currently a " + response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out! It feels like " + response.body.current.feelslike + " degrees.");
 });
+
